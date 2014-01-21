eeafbb21fc9f5fe01957d9b2feadfa99cbfa95f8
celizan <cjelizan@gmail.com>
2014-01-20 21:31:09 -0800

    Added more dependencies for InstructorDB

---
+++ implementation/source/java/schedtool/models/data/databases/CourseDB.java
@@ -0,0 +1,14 @@
+package models.data;
+
+/**
+ * Save data for the course db view.
+ * 
+ * @author Alex Kavanaugh
+ *
+ */
+public class CourseDB {
+    
+    public void save_changes() {
+        System.out.println("In CourseDBModel:save_changes()");
+    }
+}
