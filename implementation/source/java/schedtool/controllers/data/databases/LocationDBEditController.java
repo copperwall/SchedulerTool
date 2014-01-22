fb4cdd36e62cb0fad4a126fabc9c5e92e7ed2b0a
Chris Opperwall <copperwa@calpoly.edu>
2014-01-21 20:13:51 -0800

    Added Location Views/Models/Controllers
    View manipulation isn't implemented yet. But should print when trying to
    access the model. Hopefully all the controllers are setup in fxml.

---
+++ implementation/source/java/schedtool/controllers/data/databases/LocationDBEditController.java
@@ -0,0 +1,51 @@
+package controllers.data.databases;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
+public class LocationDBAddController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField buildingText;
+
+    @FXML
+    private TextField capacityText;
+
+    @FXML
+    private TextField equipmentText;
+
+    @FXML
+    private TextField roomText;
+
+
+    @FXML
+    void cancel(ActionEvent event) {
+    	// TODO Close view
+    }
+
+    @FXML
+    void submit(ActionEvent event) {
+    	// TODO Adds new row to grid view
+    }
+
+    @FXML
+    void initialize() {
+        assert buildingText != null : "fx:id=\"buildingText\" was not injected: check your FXML file 'LocationDBEditView.fxml'.";
+        assert capacityText != null : "fx:id=\"capacityText\" was not injected: check your FXML file 'LocationDBEditView.fxml'.";
+        assert equipmentText != null : "fx:id=\"equipmentText\" was not injected: check your FXML file 'LocationDBEditView.fxml'.";
+        assert roomText != null : "fx:id=\"roomText\" was not injected: check your FXML file 'LocationDBEditView.fxml'.";
+
+        // Should fill up fields with selected data. Future work perhaps
+
+
+}
