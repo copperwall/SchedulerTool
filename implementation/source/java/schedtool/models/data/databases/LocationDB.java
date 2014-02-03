39e6068e1aaf1b6b6e83a9636fb17ffcfacd4227
Chris Opperwall <copperwa@calpoly.edu>
2014-02-03 15:30:35 -0800

    Added get, add, edit, remove, and getAll methods to LocationDB
    JML coming soon. All operations are done on the private instance vector, and don't relate to model and DB _so far_.

--- implementation/source/java/schedtool/models/data/databases/LocationDB.java
+++ implementation/source/java/schedtool/models/data/databases/LocationDB.java
@@ -1,5 +1,7 @@
 package models.data.databases;
 
+import java.util.Vector;
+
 /**
  * Save data for the location db view.
  * 
@@ -7,9 +9,31 @@ package models.data.databases;
  *
  */
 public class LocationDB {
-	
-	public void saveChanges() {
-		System.out.println("In LocationDBModel:save_changes()");
-	}
+   private Vector<Location> locations;
 
+   public LocationDB() {
+      locations = new Vector<Location>();
+   }
+	
+   /* Get, Add, Edit, Delete */
+
+   public Location getLocation(int id) {
+      return locations.get(id);
+   }
+
+   public void addLocation(Location location) {
+      locations.add(location);
+   }
+
+   public void editLocation(Location location) {
+      locations.set(location.getId(), location);
+   }
+
+   public void deleteLocation(Location location) {
+      location.remove(location);
+   }
+
+   public Vector<Location> getAllLocations() {
+      return locations;
+   }
 }
