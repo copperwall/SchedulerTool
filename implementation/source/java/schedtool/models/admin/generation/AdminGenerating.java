5c94fcad416d108cef010b529f493df8f2f48ca0
Jarred Stelfox <jarstelfox@mac.com>
2014-02-07 14:30:04 -0800

    should kinda generate random input to be put into main table

--- implementation/source/java/schedtool/models/admin/generation/AdminGenerating.java
+++ implementation/source/java/schedtool/models/admin/generation/AdminGenerating.java
@@ -11,12 +11,22 @@ public class AdminGenerating {
 	 * @return a schedule.
 	 */
 	public void generate() {
+		String[] prefixes = {"CPE","CSC","MATH","AERO","BIO","EE","ME","CHEM"};
 		Schedule generatedSchedule = new Schedule();
-		//Course testCourse = new Course();
+		Course testCourse = new Course();
+		testCourse.setCourseNum((int) Math.random() * 500);
+		testCourse.setPrefix(prefixes[(int) Math.random() * prefixes.length]);
+		testCourse.setUnits(4);
+		testCourse.hasLab(false);
 		Instructor testInstructor = new Instructor();
-		//ArrayList<Day> testdays = new ArrayList<Day>();
-		//Section testSection = new Section(testCourse, 0, testInstructor, null, testdays);
-		//generatedSchedule.setOneSection(testSection);
+		testInstructor.firstName = "Gene";
+		testInstructor.lastName = "Fisher";
+		testInstructor.username = "gfisher";
+		Location testLocation = new Location(""+((int)Math.random() * 100), ""+((int)Math.random() * 200), (int)Math.random() * 50, new String[1]);
+		ArrayList<Day> testdays = new ArrayList<Day>();
+		testDays.add(new Day());
+		Section testSection = new Section(testCourse, 0, testInstructor, testLocation, testdays);
+		generatedSchedule.setOneSection(testSection);
 		System.out.println("AdminGenerating.GENERATING!!!");
 	}
 }
