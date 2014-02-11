f33b2996d540ca93805f9d271c4036aaabb598ce
Jarred Stelfox <jarstelfox@mac.com>
2014-02-11 02:07:44 -0800

    alright its bedtime since my alarm goes off in 4 hours :/

--- implementation/source/java/schedtool/scheduler_tool/DesktopMain.java
+++ implementation/source/java/schedtool/scheduler_tool/DesktopMain.java
@@ -11,9 +11,11 @@ public class DesktopMain extends Application {
     public void start(Stage primaryStage) {
         try {
             VBox root = (VBox) FXMLLoader.load(getClass().getResource("AdminFrameView.fxml"));
-            Scene scene = new Scene(root,800,600);
+            Scene scene = new Scene(root,1070,650);
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
+            primaryStage.setMinWidth(750);
+            primaryStage.setMinHeight(480);
             primaryStage.show();
         } catch(Exception e) {
             e.printStackTrace();
