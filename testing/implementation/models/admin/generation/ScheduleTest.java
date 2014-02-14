fe54d0525c9c1d576888c63064c7c6de72d8c994
Jarred Stelfox <jarstelfox@mac.com>
2014-02-14 13:48:47 -0800

    added test for schedule, not implemented yet

---
+++ testing/implementation/models/admin/generation/ScheduleTest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package implementation.models.admin.generation;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jarred
+ *
+ */
+public class ScheduleTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link models.admin.generation.Schedule#getSections()}.
+	 */
+	@Test
+	public void testGetSections() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link models.admin.generation.Schedule#setAllSections(java.util.Vector)}.
+	 */
+	@Test
+	public void testSetAllSections() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link models.admin.generation.Schedule#getAllSections()}.
+	 */
+	@Test
+	public void testGetAllSections() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link models.admin.generation.Schedule#setOneSection(models.admin.generation.Section)}.
+	 */
+	@Test
+	public void testSetOneSection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link models.admin.generation.Schedule#getOneSection(int, models.data.databases.Course)}.
+	 */
+	@Test
+	public void testGetOneSection() {
+		fail("Not yet implemented");
+	}
+
+}
