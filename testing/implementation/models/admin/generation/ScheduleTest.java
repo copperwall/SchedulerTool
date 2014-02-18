ddd04ddd93064e5e668167de96779d055fc678c2
Jarred Stelfox <jarstelfox@mac.com>
2014-02-17 22:26:53 -0800

    one test implemented

--- testing/implementation/models/admin/generation/ScheduleTest.java
+++ testing/implementation/models/admin/generation/ScheduleTest.java
@@ -4,6 +4,10 @@
 package implementation.models.admin.generation;
 
 import static org.junit.Assert.*;
+import models.admin.generation.Schedule;
+import models.admin.generation.Section;
+import models.data.databases.Instructor;
+import models.data.databases.Location;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -42,7 +46,18 @@ public class ScheduleTest {
 	 */
 	@Test
 	public void testGetSections() {
-		fail("Not yet implemented");
+		Schedule schedule = new Schedule();
+		boolean hasLab = false;
+		boolean isLab = false;
+		int count = 0;
+		models.data.databases.Course testCourse = new models.data.databases.Course("CSC", 101, hasLab, 4, "");
+		models.data.databases.Instructor testInstructor = new models.data.databases.Instructor("Gene", "Fisher", 15, true);
+		testInstructor.username = "gfisher";
+		Location testLocation = new Location(""+14, ""+256, 50, new String[1]);
+		Section testSection = new Section(testCourse, count++, testInstructor, testLocation, "MWF", 2, 4);
+		testSection.setEnrolled(50);
+		schedule.setOneSection(testSection);
+		assertEquals(schedule.getAllSections().size(),1);
 	}
 
 	/**
