83e43f1093301cb150bd5db47374e7d46e7ce804
celizan <cjelizan@gmail.com>
2014-02-19 15:41:18 -0800

    more testing
    Go Nagai is the fucking man

---
+++ testing/implementation/models/data/databases/InstructorTest.java
@@ -0,0 +1,102 @@
+package models.data.databases;
+import static org.junit.Assert.*;
+
+import java.awt.List;
+import java.util.Vector;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test class for <a href="InstructorDB.html">Instructor</a>. It implements the
+ * following test plan:
+ *                                                                        <ul>
+ *                                                                      <p><li>
+ *     Phase 1: Unit test the constructor.
+ *                                                                      <p><li>
+ *     Phase 2: Unit test getting class preferences
+ *                                                                      <p><li>
+ *     Phase 3: Unit test setting class preferences
+ *                                                                        </ul>
+ * @author Calvin Elizan
+ *
+ */
+public class InstructorTest {
+	@Before
+	public void setUp()
+	{
+	}
+	
+	/**
+	 * Unit test the constructor for an Instructor. Only one kind of instructor,
+	 * so it only needs one test.
+	 * 																			<pre>
+	 *  Test
+     *  Case    Input                    Output             Remarks
+     * ====================================================================
+     *   1      {"Test Name", "tname"    Proper init done   Only case
+     *           10, false}
+     *                                                                         </pre>
+	 */
+	@Test
+	public void testConstructor()
+	{
+		Instructor test = new Instructor("Test Name", "tname", 10, false);
+		
+		assertTrue(test != null);
+	}
+	
+	/**
+	 * Unit test getting an instructor's user name
+	 * 																			<pre>
+	 *  Test
+     *  Case    Input                    Output             Remarks
+     * ====================================================================
+     *   1      {"Test Name", "tname"    "tname"            Only case
+     *           10, false}
+     *                                                                         </pre>
+	 */
+	@Test
+	public void testGetUserID()
+	{
+		Instructor test = new Instructor("Test Name", "tname", 10, false);
+		
+		assertTrue(test.getUser().equals("tname"));
+	}
+	
+	/**
+	 * Unit test getting an instructor's user name.
+	 * 																			<pre>
+	 *  Test
+     *  Case    Input                    Output             Remarks
+     * ====================================================================
+     *   1      {"Test Name", "tname"    10                 Only case
+     *           10, false}
+     *                                                                         </pre>
+	 */
+	@Test
+	public void testGetWTU()
+	{
+		Instructor test = new Instructor("Test Name", "tname", 10, false);
+		
+		assertTrue(test.getWtu() == 10);
+	}
+	
+	/**
+	 * Unit test getting course preferences.
+	 * 																			<pre>
+	 *  Test
+     *  Case    Input                    Output               Remarks
+     * ======================================================================
+     *   1      {"Test Name", "tname"    all default course   gets all course
+     *           10, false}              prefs                preferences
+     *                                                                         </pre>
+	 */
+	@Test
+	public void testGettingPrefs()
+	{
+		Instructor test = new Instructor("Test Name", "tname", 10, false);
+		
+		assertTrue(new Vector<CoursePreference>().containsAll(test.getAllClassPrefs()));
+	}
+}
