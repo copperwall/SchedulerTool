2b4623270b29acc358a1db7703ea7bc3ed24af9f
Chase Kragenbrink <ckragenbrink@gmail.com>
2014-02-24 15:55:48 -0800

    Added grid pane

--- implementation/source/java/schedtool/controllers/admin/generation/InstructorPreferencesController.java
+++ implementation/source/java/schedtool/controllers/admin/generation/InstructorPreferencesController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.GridPane;
 import javafx.collections.ObservableList;
@@ -18,13 +19,16 @@ import models.data.databases.*;
 public class InstructorPreferencesController {
 
    @FXML
-   private TableView<?> instructorListTable;
+   private TableView<Instructor> instructorListTable;
 
    @FXML
    private CheckBox lockInstructorPrefsCheckbox;
 
    @FXML
    private GridPane timePreferencesTable;
+   
+   @FXML
+   private GridPane coursePreferencesGridPane;
    
    @FXML
    void initialize() {
@@ -47,11 +51,39 @@ public class InstructorPreferencesController {
          
       }
       
+      
+      
    }
 
    @FXML
    void onLockChanged(ActionEvent event) {
       new InstructorDB().editInstructor(null);
+   }
+   
+   private void addCoursePrefRow(CoursePreference course, int rowIndex) {
+      //coursePreferencesGridPane.getChildren().clear();
+      int columnIndex = 0;
+      Label prefix = new Label(course.course.getPrefix());
+      coursePreferencesGridPane.add(prefix, columnIndex++, rowIndex);
+      
+      Label courseNo = new Label(String.valueOf(course.course.getCourseNum()));
+      coursePreferencesGridPane.add(courseNo, columnIndex++, rowIndex);
+      
+      CheckBox notQualified = new CheckBox();
+      notQualified.setSelected(course.preference == 0);
+      coursePreferencesGridPane.add(notQualified, columnIndex++, rowIndex);
+      
+      Slider slider = new Slider();
+      slider.setBlockIncrement(1.0);
+      slider.setMajorTickUnit(1.0);
+      slider.setMax(5.0);
+      slider.setMin(1.0);
+      slider.setMinorTickCount(0);
+      slider.setShowTickMarks(true);
+      slider.setSnapToTicks(true);
+      slider.setValue(course.preference); 
+      
+      coursePreferencesGridPane.add(slider, columnIndex, rowIndex);
    }
 
 }
